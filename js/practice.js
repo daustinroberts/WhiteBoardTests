ba02c6128cfa1c7a4c5342d2ad52e52e9fa4640f
Austin Roberts <d.austin.roberts@gmail.com>
2018-07-19 16:39:40 -0700

    adding user input

--- js/practice.js
+++ js/practice.js
@@ -1,6 +1,8 @@
 
 function strFunction() {
-  var string = "Hello everyone";
-  var newStr = string.split(" ").reverse();
-  input=document.getElementById("str").innerHTML
+  var input ="";
+  input=document.getElementById("userInput").innerHTML.value;
+  console.log(input)
+  var newStr = input.split(" ").reverse();
+  document.getElementById("str").innerHTML = newStr;
 }
